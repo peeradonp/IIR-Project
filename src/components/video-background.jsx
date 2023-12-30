0bb6b1fdf70cde5d4e3cf0fa1df41e92e1f2f3ac
peeradon <peeradop@scg.com>
2023-12-30 23:33:56 +0700

    fix moblie now show vdo

--- src/components/video-background.jsx
+++ src/components/video-background.jsx
@@ -1,3 +1,5 @@
+// VideoBackground.js
+
 import React, { useRef, useEffect } from "react"
 
 const VideoBackground = () => {
@@ -7,20 +9,31 @@ const VideoBackground = () => {
         const video = videoRef.current
 
         const handleCanPlay = () => {
+            // Ensure the video is muted for autoplay to work on mobile
+            video.muted = true
             video.play().catch((error) => console.error("Error playing video:", error))
         }
 
         video.addEventListener("canplay", handleCanPlay)
 
+        // Mobile browsers may require user interaction to autoplay videos
+        const handleInteraction = () => {
+            video.play().catch((error) => console.error("Error playing video:", error))
+            document.removeEventListener("touchstart", handleInteraction)
+        }
+
+        document.addEventListener("touchstart", handleInteraction)
+
         return () => {
             video.removeEventListener("canplay", handleCanPlay)
+            document.removeEventListener("touchstart", handleInteraction)
         }
     }, [])
 
     return (
-        <div style={{ height: "100lvh" }}>
+        <div className='video-background'>
             <video ref={videoRef} className='video' loop muted playsInline>
-                <source src='1080p.mp4' type='video/mp4' />
+                <source src='your-video.mp4' type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
         </div>
